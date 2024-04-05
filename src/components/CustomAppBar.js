db57f7edd5ea8af71cb566111eca5f6080bb90b7
rainev <123475834+rainev@users.noreply.github.com>
2024-04-05 15:08:53 +0800

    Custom App Change

--- src/components/CustomAppBar.js
+++ src/components/CustomAppBar.js
@@ -5,17 +5,17 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useUser } from './UserContext';
 function CustomAppBar({ handleOpenUserMenu, handleCloseUserMenu, anchorElUser,}) {
   const navigate = useNavigate();
-
-  // const handleReserveSeatClick = () => {
-  //   console.log("Profile: ", user.profile.username);
-  //   if (user.profile.username === 'admin') {
-  //     navigate('/adminReserve');
-  //   } else {
-  //     navigate('/reserve');
-  //   }
-  // };
-
   const { user } = useUser();
+  const handleReserveSeatClick = () => {
+    console.log("Profile: ", user.profile.username);
+    if (user.profile.username === 'admin') {
+      navigate('/adminReserve');
+    } else {
+      navigate('/reserve');
+    }
+  };
+
+  
 
   return (
     <AppBar position="static" color="default" elevation={0}>
@@ -24,7 +24,7 @@ function CustomAppBar({ handleOpenUserMenu, handleCloseUserMenu, anchorElUser,})
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <img src={require('../images/Header-logo.jpg')} alt="Logo" style={{ marginRight: '20px', width: '50px', borderRadius: '50%' }} />
             <Button component={Link} to="/" color="inherit">DigitalLabs</Button>
-            <Button component={Link} to="/reserve" color="inherit">Reserve</Button>
+            <Button color="inherit" onClick={handleReserveSeatClick}>Reserve</Button>
           </Box>
           <Button color="inherit" onClick={handleOpenUserMenu}>{user.profile.name}</Button>
           <Menu
@@ -44,7 +44,7 @@ function CustomAppBar({ handleOpenUserMenu, handleCloseUserMenu, anchorElUser,})
             onClose={handleCloseUserMenu}
           >
             <MenuItem component={Link} to="/profile" onClick={handleCloseUserMenu}>Profile</MenuItem>
-            <MenuItem component={Link} to="/reserve" onClick={handleCloseUserMenu}>Reserve</MenuItem>
+            <MenuItem onClick={handleReserveSeatClick}>Reserve</MenuItem>
             <MenuItem component={Link} to="/login" onClick={handleCloseUserMenu}>Logout</MenuItem>
           </Menu>
         </Container>
